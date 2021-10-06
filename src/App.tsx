d8445167f1a751e869eeea4247382b3f003c748a
Vinith-arun <vinith.p@tringapps.com>
2021-10-06 21:00:10 +0530

    commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React ,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState{
+  people:{
+    age: number,
+    name: string,
+    url: string,
+    note?: string
+  }[]
+}
+ 
+function App() {
+  const [people, setPeople]= useState <IState["people"]>([])
+  return (
+    <div className="App">
+      <h1>People invited to my Party</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
+    </div>
+  );
+}
+
+export default App;
